3103e411d62a6a670f10eb58555d9d5a619e5420
Maina <75148285+ishakuyusufmaina@users.noreply.github.com>
2026-02-13 15:09:16 +0100

    Add Firebase configuration for student app
    Added Firebase configuration for student app.

--- config.js
+++ config.js
@@ -1,2 +1,15 @@
+const studentConfig =  {
+  apiKey: "AIzaSyAh1dfz_La25h9EQ8SwSkq9WCRrBAaNpoM",
+  authDomain: "dh-students.firebaseapp.com",
+  projectId: "dh-students",
+  storageBucket: "dh-students.firebasestorage.app",
+  messagingSenderId: "534789912463",
+  appId: "1:534789912463:web:fad50e1664966a0b843dfa",
+  measurementId: "G-WK9KF8PB2W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const schoolId = "dhacademy";
 const schoolName = "DEVELOPMENT HUB ACADEMY";
