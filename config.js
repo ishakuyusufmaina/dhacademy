cbb70d5c5acc4de2bafd8858207573b920013161
Maina <75148285+ishakuyusufmaina@users.noreply.github.com>
2026-02-25 11:58:11 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -8,8 +8,28 @@ const studentsConfig =  {
   measurementId: "G-WK9KF8PB2W"
 };
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDYco_QRpPXc8J5NuCMBbnpWgOL2XCSbcI",
+  authDomain: "sib-batch3.firebaseapp.com",
+  projectId: "sib-batch3",
+  storageBucket: "sib-batch3.firebasestorage.app",
+  messagingSenderId: "931289126392",
+  appId: "1:931289126392:web:491c203114eccb7c19320d",
+  measurementId: "G-66NXM5YSZ1"
+};
+
+
+const paybookConfig = {
+    apiKey: "AIzaSyAwrWSv_pD2keej4qIjx1jeeRq5K_DBKYA",
+    authDomain: "mainafly-school-fees.firebaseapp.com",
+    projectId: "mainafly-school-fees",
+    storageBucket: "mainafly-school-fees.firebasestorage.app",
+    messagingSenderId: "813090872440",
+    appId: "1:813090872440:web:b1b1bc39ab82256fa2a18d",
+    measurementId: "G-E6B3WQ6P8Q"
+  };
+
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 const schoolId = "dhacademy";
 const schoolName = "DEVELOPMENT HUB ACADEMY";
